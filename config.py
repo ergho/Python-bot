3590bd9b0732a7612aaede2ad8d5b2664d9c7fe4
ergho <lazertarget@gmail.com>
2020-03-23 23:46:37 +0100

    Not needed anymore

--- config.py
+++
@@ -1,9 +0,0 @@
-import toml
-
-#Simple parsing of toml config file, returns dictionaries of section or subsection
-
-def config(filename, section, subsection = None):
-    parser = toml.load(filename)
-    if subsection is None:
-        return parser[section]
-    return parser[section][subsection]
