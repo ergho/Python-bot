901eff018aa58ef33f645c0c4a37e28c732676e9
ergho <lazertarget@gmail.com>
2020-03-28 05:09:48 +0100

    added a 1hour sleep to query the api for updates might change that later

--- cogs/poe.py
+++ cogs/poe.py
@@ -8,9 +8,10 @@ class Pathofexile:
         #self.league = await self.bot.db.execute
 
     async def event_ready(self):
-        print('m888888')
+        print('poe.cog loaded')
+        #might move this elsewhere, or change table structure
         await self.bot.db.execute("CREATE SCHEMA IF NOT EXISTS poe")
-        print('seeeefearion')
+
         await self.bot.db.execute(
                 """CREATE TABLE IF NOT EXISTS poe.store (
                 timestamp       TIMESTAMPTZ PRIMARY KEY DEFAULT NOW(),
@@ -21,12 +22,7 @@ class Pathofexile:
                 )
                 """
             )
-        await self.get_cur()
-    @commands.command(name = 'tested')
-    async def test(self, ctx):
-        print('Wow this worked? now?!')
-        
-        print('poe')
+        await self.get_cur([4,5])
     @commands.command(aliases= ('price',))
     async def get_currency_price(self, ctx, detailed_name:str, league:str):
         league = league.lstrip()
@@ -55,46 +51,56 @@ class Pathofexile:
 
     async def get_cur(self, league_ids:list):
         """
-        param: league_ids:: takes a list of 1 or more of these numbers 0 = Standard, 1 = Hardcore, 4= Temp, 5 = Hardcore Temp
+        param: league_ids:: takes a list of numbers
+        0 = Standard, 1 = Hardcore, 4= Temp, 5 = Hardcore Temp
         """
         league = await self.get_leagues(league_ids)
-        types = ['Currency', 'Fragment', 'Oil', 'Incubator', 'Scarab', 'Fossil', 'Resonator', 'Essence', 'DivinationCard', 'Prophecy', 'BaseType', 'UniqueMap', 'Map', 'UniqueJewel', 'UniqueFlask', 'UniqueWeapon', 'UniqueArmour', 'UniqueAccessory']
+        types = ['Currency', 'Fragment', 'Oil', 'Incubator', 'Scarab', 'Fossil', 'Resonator', 
+        'Essence', 'DivinationCard', 'Prophecy', 'BaseType', 'UniqueMap', 'Map', 'UniqueJewel', 
+        'UniqueFlask', 'UniqueWeapon', 'UniqueArmour', 'UniqueAccessory']
         base = ['currencyoverview', 'itemoverview']
-        for i in league:
-            for x in base:
-                for y in types:
-                    #stops inherently faulty requests, this will do for now.
-                    if x == base[1] and y == types[0] or x == base[1] and y == types[1]:
-                        continue
-                    url = f'https://poe.ninja/api/data/{x}?league={i}&type={y}'
-                    async with self.bot.aiohttp_session.get(url) as resp:
-                        if resp.status != 200:
-                            #todo add loggging of errors instead of just skipping over?
+        while True:
+            for i in league:
+                for x in base:
+                    for y in types:
+                        #stops inherently faulty requests, before sending them
+                        if x == base[1] and y == types[0] or x == base[1] and y == types[1]:
                             continue
-                        currencies = await resp.json()
-                        for t in currencies['lines']:                        
-                            if x == base[0]
-                                if t['chaosEquivalent'] > 5:
-                                    await self.bot.db.execute(
-                                        """
-                                        INSERT INTO poe.currency (timestamp, league, name, detailed_name, chaosvalue)
-                                        VALUES ($1, $2, $3, $4 $5)
-                                        """,
-                                        datetime.datetime.now(), i, t['currencyTypeName'], t['detailsId'], t['chaosEquivalent']
-                                    )
-                            elif x == base[1]:
-                                if t['chaosValue'] > 5:
-                                    await self.bot.db.execute(
-                                        """
-                                        INSERT INTO poe.currency (timestamp, league, name, detailed_name, chaosvalue)
-                                        VALUES ($1, $2, $3, $4, $5)
-                                        """,
-                                        datetime.datetime.now(), i, t['name'], t['detailsId'], t['chaosValue']
-                                    )
+                        url = f'https://poe.ninja/api/data/{x}?'
+                        params = {'league':i, 'type': y}
+                        async with self.bot.aiohttp_session.get(url, params=params) as resp:
+                            if resp.status != 200:
+                                #todo add loggging of errors instead of just skipping over?
+                                continue
+                            currencies = await resp.json()
+                            for t in currencies['lines']:                        
+                                if x == base[0]:
+                                    if t['chaosEquivalent'] > 5:
+                                        await self.bot.db.execute(
+                                            """
+                                            INSERT INTO poe.currency (timestamp, league, name, detailed_name, chaosvalue)
+                                            VALUES ($1, $2, $3, $4 $5)
+                                            """,
+                                            datetime.datetime.now(), i, t['currencyTypeName'], t['detailsId'], t['chaosEquivalent']
+                                        )
+                                elif x == base[1]:
+                                    if t['chaosValue'] > 5:
+                                        await self.bot.db.execute(
+                                            """
+                                            INSERT INTO poe.currency (timestamp, league, name, detailed_name, chaosvalue)
+                                            VALUES ($1, $2, $3, $4, $5)
+                                            """,
+                                            datetime.datetime.now(), i, t['name'], t['detailsId'], t['chaosValue']
+                                        )
+            await asyncio.sleep(3600)
+    
+### pasuse for now
+    # async def trade_url(self):
 
-#    async def get_tiny_url(self):
+    # async def get_tiny_url(self, trade:str):
+    #     url = 'http://tinyurl.com/api-create.php?'
+        
+
 
 # #To do add in aiohttp for using poe.ninja and pathofexile api for grabbing currency information
 # #then storing the data somehow, psql? or keyvalue? or other ideas
-
-        
