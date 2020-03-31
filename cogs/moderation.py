a0f95202a1fd73f35577f45c9ab09883289f3f24
ergho <lazertarget@gmail.com>
2020-03-31 06:49:38 +0200

    made a weird implementation of a permit list to temporarily allow for people to be allowed to do things.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,20 +8,39 @@ class Moderation:
     """
     def __init__(self, bot):
         self.bot = bot
+        self.allowed_users = []
+    async def event_message(self, ctx):
+        print('hi')
+
+    @commands.command(aliases = ('permit',))
+    async def permit_link(self, ctx, username: str, duration:int = 60):
+        if ctx.author.is_mod == 1:
+            eval_username = await self.bot.db.fetchval(
+            """
+            SELECT username FROM twitch.users  
+            WHERE username = $1 AND channel = $2)
+            """,
+            username.lower(), ctx.channel.name)
+            if username != eval_username:
+                await ctx.send(f"@{ctx.channel.author}, check the spelling of {username}")
+                return
+            await self.permit_users(username, 'permit')
+            await self.bot.asyncio.sleep(duration)
+            await self.permit_users(username, 'remove')
+        else:
+            await ctx.send('Mod only command!')
 
     @commands.command(aliases = ('timeout',))
-    async def timeout_user(self, ctx, username, duration: int, reason: str = None):
+    async def timeout_user(self, ctx, username: str, duration: int, reason: str = None):
         if ctx.author.is_mod == 1:
             #to do add user to banlist in database with reason and timeout user for duration.
-            pass
             await ctx.channel.timeout(username, duration, reason)
         else:
             await ctx.send('Mod only command!')
 
     @commands.command(aliases = ('ban',))
-    async def ban_user(self, ctx, username: str, reason : str = None, duration : str = 'Permanent'):
+    async def ban_user(self, ctx, username: str, reason: str = None, duration: str = 'Permanent'):
         if ctx.author.is_mod == 1:
-            print( username,'hello', reason, 'yep', duration )
             await self.bot.db.execute(
                 """
                 INSERT INTO twitch.banlist (timestamp, channel, username, reason, duration)
@@ -29,16 +48,14 @@ class Moderation:
                 """,
                 datetime.datetime.now(), ctx.channel.name, username, reason, duration
             )
-            print('did it')
             #to do add user to the banlist in database with reason why and ban user in chat and duration(permanent)
             await ctx.channel.ban(username, reason)
         else:
             await ctx.send('Mod only command!')
 
     @commands.command(aliases = ('unban',))
-    async def unban_user(self, ctx, username, reason : str = ''):
+    async def unban_user(self, ctx, username: str, reason: str = None):
         if ctx.author.is_mod == 1:
-            pass
             await ctx.channel.unban(username)
         else:
             await ctx.send('Mod only command!')
@@ -59,8 +76,15 @@ class Moderation:
         else:
             await ctx.send('Mod only command!')
     @commands.command(aliases = ('unban',))
-    async def unban_user(self, ctx, username, reason : str = ''):
+    async def unban_user(self, ctx, username: str, reason: str = None):
         if ctx.author.is_mod == 1:
             pass
         else:
             await ctx.send('Mod only command!')
+
+    async def permit_users(self, username:str, mode:str):
+        
+        if 'permit' == mode:
+            self.allowed_users.append(username)
+        elif 'remove' == mode:
+            self.allowed_users.remove(username)
