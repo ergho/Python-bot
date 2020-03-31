15cec89f65c46b0f694751d6073878725646b66c
ergho <lazertarget@gmail.com>
2020-03-31 06:48:29 +0200

    added aiohttp.client logging, as a try to see if it catches anything

--- utility/logging.py
+++ utility/logging.py
@@ -5,6 +5,7 @@ import logging.handlers
 Path('logs/twitchio').mkdir(parents=True, exist_ok=True)
 Path('logs/raw_data').mkdir(parents=True, exist_ok=True)
 Path('logs/asyncio').mkdir(parents=True, exist_ok=True)
+Path('logs/aiohttp').mkdir(parents=True, exist_ok=True)
 
 twitchio_logger = logging.getLogger('twitchio')
 twitchio_logger.setLevel(logging.DEBUG)
@@ -16,9 +17,16 @@ twitchio_logger.addHandler(twitchio_logger_handler)
 asyncio_logger = logging.getLogger('asyncio')
 asyncio_logger.setLevel(logging.DEBUG)
 asyncio_logger_handler = logging.handlers.TimedRotatingFileHandler(filename ='logs/asyncio/asyncio.log',
-                                                                    when = 'midnight', backupCount = 500000)
+                                                                    when = 'midnight', backupCount = 500000, encoding= 'UTF-8')
 asyncio_logger_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s %(name)s: %(message)s'))
 asyncio_logger.addHandler(asyncio_logger_handler)
+
+aiohttp_client_logger = logging.getLogger('aiohttp.client')
+aiohttp_client_logger.setLevel(logging.DEBUG)
+aiohttp_client_logger_handler = logging.handlers.TimedRotatingFileHandler(filename='logs/aiohttp/client.log',
+                                                                            when= 'midnight', backupCount=500000, encoding= 'UTF-8')
+aiohttp_client_logger_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s %(name)s: %(message)s'))
+aiohttp_client_logger.addHandler(aiohttp_client_logger_handler)
 
 raw_data_logger = logging.getLogger('raw data')
 raw_data_logger.setLevel(logging.DEBUG)
