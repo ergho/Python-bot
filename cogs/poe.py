4cce531dadf1db6c8f9c8560ef8a7f58ff251769
ergho <lazertarget@gmail.com>
2020-04-11 13:44:01 +0200

    added annotations and trying to not redefine those variables.

--- cogs/poe.py
+++ cogs/poe.py
@@ -48,27 +48,25 @@ class Pathofexile:
         param: league_ids: takes a list of 1 or more numbers
         0 = Standard, 1 = Hardcore, 4= Temp, 5 = Hardcore Temp
         """
-        url = 'https://api.pathofexile.com/leagues'
+        url: str = 'https://api.pathofexile.com/leagues'
         async with self.bot.aiohttp_session.get(url) as resp:
-            league = await resp.json()
+            league: dict = await resp.json()
             return [league[x]['id'] for x in league_ids]
 ## todo double check bugfix to see if the error to database is gone weird base types on wrong items!
     async def get_currency_from_api(self, league_ids: list, base: str, types: list, price_types: str, name_types: str):
-        url = f'https://poe.ninja/api/data/{base}?'
-        for ids in league_ids:
-            for item in types:
-                params = {'league': ids, 'type': item}
-                api_response = await self.get_json(url, **params)
-                if api_response == 0:
+        url: str = f'https://poe.ninja/api/data/{base}?'
+        for ids in league_ids: # type: int
+            for item in types: # type: str
+                params: dict = {'league': ids, 'type': item}
+                api_response: dict = await self.get_json(url, **params)
+                if api_response is None:
                     print("There's been an error")
                     continue
-                for entry in api_response['lines']:
+                for entry in api_response['lines']: # type: dict
                     base_type = 'None'
                     if 'baseType' in entry:
                         if entry['baseType']:
                             base_type = entry['baseType']
-                    # else:
-                    #      base_type = 'None'                          
                     if entry[price_types] > 50:
                         await self.bot.db.execute(
                             """
@@ -77,10 +75,9 @@ class Pathofexile:
                             """,
                             datetime.datetime.now(), ids, entry[name_types], base_type, entry['detailsId'], entry[price_types]
                             )
-    async def get_json(self, url: str, amount_of_tries: int=5, **parameters:dict):
-        api_response = None
-        tries = 0
-        delay = 0
+    async def get_json(self, url: str, amount_of_tries: int=5, **parameters:dict) -> dict:
+        api_response: dict = None
+        tries: int = 0
         if len(parameters) == 0:
             parameters = None
         while api_response is None and tries < amount_of_tries:
@@ -90,28 +87,26 @@ class Pathofexile:
                     api_response = await resp.json()
             except Exception as e:
                 tries += 1
-                delay = 2 ** tries + 1
+                delay: int = 2 ** tries + 1
                 await asyncio.sleep(delay)
-        if api_response is None:
-            return 0
         return api_response
     
-    async def get_cur(self, league_ids: list):
+    async def get_cur(self, league_ids: list) -> None:
         """
         param: league_ids:: takes a list of numbers
         0 = Standard, 1 = Hardcore, 4= Temp, 5 = Hardcore Temp
         """
-        league = await self.get_leagues(league_ids)
-        currency_types = ['Currency', 'Fragment']
-        item_types = ['Oil', 'Incubator', 'Scarab','Fossil', 'Resonator',
+        league: List[str] = await self.get_leagues(league_ids)
+        currency_types: List[str] = ['Currency', 'Fragment']
+        item_types: List[str] = ['Oil', 'Incubator', 'Scarab','Fossil', 'Resonator',
                       'Essence', 'DivinationCard', 'Prophecy', 'BaseType',
                       'UniqueMap', 'Map', 'UniqueJewel', 'UniqueFlask',
                       'UniqueWeapon', 'UniqueArmour', 'UniqueAccessory']
-        price_types = ['chaosEquivalent', 'chaosValue']
-        base = ['currencyoverview', 'itemoverview']
-        name_types = ['currencyTypeName', 'name']
+        price_types: List[str] = ['chaosEquivalent', 'chaosValue']
+        base: List[str] = ['currencyoverview', 'itemoverview']
+        name_types: List[str] = ['currencyTypeName', 'name']
         while True:
-            delay = random.randrange(3500, 3800)
+            delay: int = random.randrange(3500, 3800)
 
             await self.get_currency_from_api(league, base[0], currency_types, price_types[0], name_types[0])
             await self.get_currency_from_api(league, base[1], item_types, price_types[1], name_types[1])
@@ -119,14 +114,14 @@ class Pathofexile:
             await asyncio.sleep(delay)
 
     @commands.command(aliases=('price', 'cprice'))
-    async def get_currency_price(self, ctx, item_name: str, league: str = 'Delirium'):
-        league = league.lstrip()
-        check_league = await self.get_leagues([4,5])
+    async def get_currency_price(self, ctx, item_name: str, league: str = 'Delirium') -> None:
+        league: str = league.strip()
+        check_league: List[str] = await self.get_leagues([4,5])
         if league not in check_league:
             await ctx.send(f'@{ctx.author.name} please check your request and make sure you are using one of the temporary leagues.')
             return
 
-        value = dict(await self.bot.db.fetchrow(
+        value: dict = dict(await self.bot.db.fetchrow(
         """
         SELECT name, chaosvalue, base_type, poe.similarity(details_id, $1)  FROM poe.currency
         WHERE lower(league) = $2
@@ -138,12 +133,10 @@ class Pathofexile:
         # query = 
         # await get_json(url, **parameters)
         if value.get('similarity') < 0.3:
-            await ctx.send(f'@{ctx.author.name}, There is no item matching closely enough with your request with an estimated value of 50 chaos above.')
+            await ctx.send(f'@{ctx.author.name}, There is no item matching closely enough with your request and have an estimated value of 50 chaos or above.')
             return
-        print(type(value))
-        print(value)
     
-        await ctx.send(f'@{ctx.author.name}, {value.get("name")} current value in chaos {value.get("chaosvalue")}, placeholder for trade site link of item.')
+        await ctx.send(f'@{ctx.author.name}, {value.get("name")}: {value.get("chaosvalue")} Chaos Orbs, placeholder for trade site link of item.')
         
         
         # await self.
